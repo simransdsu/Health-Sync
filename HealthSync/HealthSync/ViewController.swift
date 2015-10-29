7f7e1782b2059e0902037d8d09be7fc14d352b5b
Simran <simran5590@gmail.com>
2015-10-28 17:20:06 -0700

    Live step counting done

--- HealthSync/HealthSync/ViewController.swift
+++ HealthSync/HealthSync/ViewController.swift
@@ -7,15 +7,48 @@
 //
 
 import UIKit
+import CoreMotion
 
 class ViewController: UIViewController {
 
     
     @IBOutlet weak var numberOfStepsLabel: UILabel!
     
+    var days:[String] = []
+    var stepsTakes:[Int] = []
+    
+    let activityManager = CMMotionActivityManager()
+    let pedoMeter = CMPedometer()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        navigationController?.navigationBar.barTintColor = UIColor(red: 96/256, green: 191/256, blue: 186/256, alpha: 1)
+        
+        let cal = NSCalendar.currentCalendar()
+        let comps = cal.components(NSCalendarUnit.Year , fromDate: NSDate())
+        comps.hour = 0
+        comps.minute = 0
+        comps.second = 0
+        let timeZone = NSTimeZone.systemTimeZone()
+        cal.timeZone = timeZone
+        
+        //let midnightOfToday = cal.dateFromComponents(comps)!
+        
+        if(CMMotionActivityManager.isActivityAvailable()) {
+            self.pedoMeter.startPedometerUpdatesFromDate(NSDate()) {
+                (data, error) in
+                if error != nil {
+                    print("There was an error obtaining pedometer data: \(error)")
+                } else {
+                    dispatch_async(dispatch_get_main_queue()) {
+                        self.numberOfStepsLabel.text = "\(data!.numberOfSteps)"
+                        //self.distanceLabel.text = "\(self.lengthFormatter.stringFromMeters(data.distance as! Double))"
+                    }
+                }
+            }        }
+        
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
