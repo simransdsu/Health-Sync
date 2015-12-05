b2f1192fde56cbf24cd06cfae1bc21fad95f4c3c
Girish <girish02c@gmail.com>
2015-12-05 12:07:20 -0800

    saved everything

--- HealthSync/HealthSync/FitBitManager.swift
+++ HealthSync/HealthSync/FitBitManager.swift
@@ -37,7 +37,27 @@ class FitBitManager {
     
     func getProfileData(){
         
-        let profileURL = BASE_RESOURCE_URL+"/profile.json"
+                let profileURL = BASE_RESOURCE_URL+"/profile.json"
+        
+                let header = ["Authorization":"Bearer " + FitBitCredentials.sharedInstance.fitBitValueForKey("accessToken")!]
+        
+                Alamofire.request(.GET, profileURL,headers:header).responseJSON{ response in
+                    guard response.result.error == nil else {
+                        print(response.result.error!)
+                        return
+                    }
+        
+                    if let value: AnyObject = response.result.value {
+                        print(value)
+                    }
+                }
+        
+    }
+    
+    
+    func getFitbitSteps(){
+        
+        let profileURL = BASE_RESOURCE_URL+"/activities/steps/date/today/1d.json"
         
         let header = ["Authorization":"Bearer " + FitBitCredentials.sharedInstance.fitBitValueForKey("accessToken")!]
         
@@ -53,8 +73,36 @@ class FitBitManager {
         }
         
     }
+
     
     
+    
+    func syncStepsWithFitbit(){
+        let header = ["Authorization":"Bearer " + FitBitCredentials.sharedInstance.fitBitValueForKey("accessToken")!]
+        
+        var parameter = Dictionary<String, AnyObject>()
+        
+        parameter["activityId"] = "90009"
+        parameter["startTime"] = "20:27:25"
+        parameter["durationMillis"] = "500000"
+        parameter["date"] = "2015-11-22"
+        parameter["distance"] = "2000"
+        parameter["distanceUnit"] = "Steps"
+        
+        
+        let urlstring =  BASE_RESOURCE_URL+"/activities.json";
+        
+        Alamofire.request(.POST, urlstring,parameters: parameter,headers:header).responseJSON{ response in
+            guard response.result.error == nil else {
+                print(response.result.error!)
+                return
+            }
+            
+            if let value: AnyObject = response.result.value {
+                print(value)
+            }
+        }
+    }
     
     
 }
